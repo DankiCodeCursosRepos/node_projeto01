97685ffc7cbdda99170dcd61bdd037f2d6bccb77
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-01-06 15:54:33 -0300

    Finalização do projeto

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
 const path = require('path');
 
 const app = express();
+
+
+//body parser config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 //ejs configs//
 app.engine('html', require('ejs').renderFile);
@@ -10,11 +19,30 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 
+var tarefas = ['Arrumar o quarto', 'Fazer compras no Supermercado']
+
+
 
 app.get('/', (req, res) => {
+  res.render('index',{tarefasList : tarefas});
+})
 
-  res.render('index',{nome:'Artur', idade: 33});
+app.post('/', (req, res) => {
+  // console.log(req.body.tarefa)
+  tarefas.push(req.body.tarefa)
 
+  // res.render('index',{tarefasList : tarefas});
+})
+
+app.get('/deletar/:id', (req,res) => {
+
+  tarefas = tarefas.filter((value , index) => {
+    if (index != req.params.id) {
+      return value
+    }
+  })
+
+  res.render('index',{tarefasList : tarefas});
 })
 
 app.listen(3000, ()=> {
